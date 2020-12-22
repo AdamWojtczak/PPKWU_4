8cedee01b1d44abf7c6e7956ef3ac5bfb147d41d
AdamWojtczak <adamwojtczak807@gmail.com>
2020-12-22 15:03:55 +0100

    Deleted tryouts of JSP forms. Bug with url resolved.

--- demo/src/main/java/com/example/demo/VCardGetter.java
+++ demo/src/main/java/com/example/demo/VCardGetter.java
@@ -24,89 +24,6 @@ import java.util.List;
 @RestController
 public class VCardGetter {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView showHomePage() {
-//		ModelAndView mnv = new ModelAndView("company", "company", new Company());
-//		ModelAndView mnv = new ModelAndView("company");
-//		mnv.addObject("company", new Company());
-//		System.out.println(mnv.getModel());
-//		System.out.println(mnv.getView());
-		ModelAndView mnv = new ModelAndView("index", "company", new Company());
-		return mnv;
-	}
-
-	@RequestMapping(value = "/search", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE + "; charset=utf-8")
-	public String searchEmployee( @ModelAttribute("company") Company company, BindingResult result, ModelMap model) throws IOException {
-
-		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append("https://panoramafirm.pl/szukaj?k=");
-		stringBuilder.append("hydraulik");
-		stringBuilder.append("&l=");
-		Document doc = Jsoup.connect(stringBuilder.toString()).get();
-
-		StringBuilder str = new StringBuilder();
-		List<Company> companies = new ArrayList<Company>();
-
-		System.out.println(company);
-		Elements elements = doc.select("script");
-		for (Element element : elements) {
-			if (element.attr("type").equals("application/ld+json")) {
-				if (element.data().contains("LocalBusiness")) {
-//					Company company = new Company();
-					company.name = getFieldFromJSONString(element.data(), "name");
-					company.telephone = getFieldFromJSONString(element.data(), "telephone");
-					company.email = getFieldFromJSONString(element.data(), "email");
-					company.postalCode = getFieldFromJSONString(element.data(), "postalCode");
-					company.country = getFieldFromJSONString(element.data(), "addressCountry");
-					company.street = getFieldFromJSONString(element.data(), "streetAddress");
-					company.locality = getFieldFromJSONString(element.data(), "addressLocality");
-					companies.add(company);
-					str.append(company.toString());
-					System.out.println(company.toString());
-				}
-			}
-		}
-
-		model.addAttribute("companies", companies);
-
-		return "results";
-	}
-
-	@RequestMapping(method = RequestMethod.GET, path = "/h")
-	public String hydraulik(@RequestParam("name") String profesja) throws IOException {
-		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append("https://panoramafirm.pl/szukaj?k=");
-		stringBuilder.append(profesja);
-		stringBuilder.append("&l=");
-		Document doc = Jsoup.connect(stringBuilder.toString()).get();
-
-		StringBuilder str = new StringBuilder();
-		List<Company> companies = new ArrayList<Company>();
-
-		Elements elements = doc.select("script");
-		for (Element element : elements) {
-			if (element.attr("type").equals("application/ld+json")) {
-				if (element.data().contains("LocalBusiness")) {
-					Company company = new Company();
-					company.name = getFieldFromJSONString(element.data(), "name");
-					company.telephone = getFieldFromJSONString(element.data(), "telephone");
-					company.email = getFieldFromJSONString(element.data(), "email");
-					company.postalCode = getFieldFromJSONString(element.data(), "postalCode");
-					company.country = getFieldFromJSONString(element.data(), "addressCountry");
-					company.street = getFieldFromJSONString(element.data(), "streetAddress");
-					company.locality = getFieldFromJSONString(element.data(), "addressLocality");
-					companies.add(company);
-					str.append(company.toString());
-					System.out.println(company.toString());
-				}
-			}
-		}
-
-
-
-		return str.toString();
-	}
-
 	@RequestMapping(method = RequestMethod.GET, path = "/getvcard")
 	public String getVCard(@RequestParam("name") String profesja, @RequestParam("index") String index, HttpServletResponse response) throws IOException {
 		StringBuilder stringBuilder = new StringBuilder();
@@ -130,7 +47,7 @@ public class VCardGetter {
 					company.country = getFieldFromJSONString(element.data(), "addressCountry");
 					company.street = getFieldFromJSONString(element.data(), "streetAddress");
 					company.locality = getFieldFromJSONString(element.data(), "addressLocality");
-					company.url = getFieldFromJSONString(element.data(), "url");
+					company.url = getFieldFromJSONString(element.data(), "sameAs");
 					companies.add(company);
 					str.append(company.toString());
 					System.out.println(company.toString());
@@ -171,7 +88,7 @@ public class VCardGetter {
 		}
 
 
-		return str.toString();
+		return "";
 	}
 
 	private String getFieldFromJSONString(String json, String field) {
