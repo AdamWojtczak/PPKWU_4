d6fdbd31eb07c4bbf100e4160c98f09a8709ebc4
AdamWojtczak <adamwojtczak807@gmail.com>
2020-12-22 14:24:10 +0100

    Added search by parameters functionality

--- demo/src/main/java/com/example/demo/Company.java
+++ demo/src/main/java/com/example/demo/Company.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+
 public class Company {
+	String url;
 	String name;
 	String telephone;
 	String email;
@@ -12,5 +14,69 @@ public class Company {
 	@Override
 	public String toString() {
 		return "Company{" + "name='" + name + '\'' + ", telephone='" + telephone + '\'' + ", email='" + email + '\'' + ", postalCode='" + postalCode + '\'' + ", street='" + street + '\'' + ", locality='" + locality + '\'' + ", country='" + country + '\'' + '}';
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public String getLocality() {
+		return locality;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
 	}
 }
--- demo/src/main/java/com/example/demo/VCardGetter.java
+++ demo/src/main/java/com/example/demo/VCardGetter.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +26,54 @@ public class VCardGetter {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView showHomePage() {
-		return new ModelAndView("Company", "Company", new Company());
+//		ModelAndView mnv = new ModelAndView("company", "company", new Company());
+//		ModelAndView mnv = new ModelAndView("company");
+//		mnv.addObject("company", new Company());
+//		System.out.println(mnv.getModel());
+//		System.out.println(mnv.getView());
+		ModelAndView mnv = new ModelAndView("index", "company", new Company());
+		return mnv;
+	}
+
+	@RequestMapping(value = "/search", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE + "; charset=utf-8")
+	public String searchEmployee( @ModelAttribute("company") Company company, BindingResult result, ModelMap model) throws IOException {
+
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("https://panoramafirm.pl/szukaj?k=");
+		stringBuilder.append("hydraulik");
+		stringBuilder.append("&l=");
+		Document doc = Jsoup.connect(stringBuilder.toString()).get();
+
+		StringBuilder str = new StringBuilder();
+		List<Company> companies = new ArrayList<Company>();
+
+		System.out.println(company);
+		Elements elements = doc.select("script");
+		for (Element element : elements) {
+			if (element.attr("type").equals("application/ld+json")) {
+				if (element.data().contains("LocalBusiness")) {
+//					Company company = new Company();
+					company.name = getFieldFromJSONString(element.data(), "name");
+					company.telephone = getFieldFromJSONString(element.data(), "telephone");
+					company.email = getFieldFromJSONString(element.data(), "email");
+					company.postalCode = getFieldFromJSONString(element.data(), "postalCode");
+					company.country = getFieldFromJSONString(element.data(), "addressCountry");
+					company.street = getFieldFromJSONString(element.data(), "streetAddress");
+					company.locality = getFieldFromJSONString(element.data(), "addressLocality");
+					companies.add(company);
+					str.append(company.toString());
+					System.out.println(company.toString());
+				}
+			}
+		}
+
+		model.addAttribute("companies", companies);
+
+		return "results";
 	}
 
 	@RequestMapping(method = RequestMethod.GET, path = "/h")
-	public String hydraulik(@RequestParam("p") String profesja) throws IOException {
+	public String hydraulik(@RequestParam("name") String profesja) throws IOException {
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("https://panoramafirm.pl/szukaj?k=");
 		stringBuilder.append(profesja);
@@ -34,7 +87,6 @@ public class VCardGetter {
 		for (Element element : elements) {
 			if (element.attr("type").equals("application/ld+json")) {
 				if (element.data().contains("LocalBusiness")) {
-//					System.out.println(element.data());
 					Company company = new Company();
 					company.name = getFieldFromJSONString(element.data(), "name");
 					company.telephone = getFieldFromJSONString(element.data(), "telephone");
@@ -44,13 +96,80 @@ public class VCardGetter {
 					company.street = getFieldFromJSONString(element.data(), "streetAddress");
 					company.locality = getFieldFromJSONString(element.data(), "addressLocality");
 					companies.add(company);
-//					getFieldFromJSONString(element.data(), "name");
-//					str.append(element.data());
 					str.append(company.toString());
 					System.out.println(company.toString());
 				}
 			}
 		}
+
+
+
+		return str.toString();
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "/getvcard")
+	public String getVCard(@RequestParam("name") String profesja, @RequestParam("index") String index, HttpServletResponse response) throws IOException {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("https://panoramafirm.pl/szukaj?k=");
+		stringBuilder.append(profesja);
+		stringBuilder.append("&l=");
+		Document doc = Jsoup.connect(stringBuilder.toString()).get();
+
+		StringBuilder str = new StringBuilder();
+		List<Company> companies = new ArrayList<Company>();
+
+		Elements elements = doc.select("script");
+		for (Element element : elements) {
+			if (element.attr("type").equals("application/ld+json")) {
+				if (element.data().contains("LocalBusiness")) {
+					Company company = new Company();
+					company.name = getFieldFromJSONString(element.data(), "name");
+					company.telephone = getFieldFromJSONString(element.data(), "telephone");
+					company.email = getFieldFromJSONString(element.data(), "email");
+					company.postalCode = getFieldFromJSONString(element.data(), "postalCode");
+					company.country = getFieldFromJSONString(element.data(), "addressCountry");
+					company.street = getFieldFromJSONString(element.data(), "streetAddress");
+					company.locality = getFieldFromJSONString(element.data(), "addressLocality");
+					company.url = getFieldFromJSONString(element.data(), "url");
+					companies.add(company);
+					str.append(company.toString());
+					System.out.println(company.toString());
+				}
+			}
+		}
+
+
+		Company indexedCompany = companies.get(Integer.parseInt(index));
+
+		if(indexedCompany == null) {
+			return "Podano zły indeks";
+		}
+
+		File file = new File("vcard.vcf");
+		FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+		bufferedWriter.write("BEGIN:VCARD\r\n");
+		bufferedWriter.write("VERSION:4.0\r\n");
+		bufferedWriter.write("ORG:" + indexedCompany.getName() + "\r\n");
+		bufferedWriter.write("TEL:" + indexedCompany.getTelephone() + "\r\n");
+		bufferedWriter.write("ADR:" + indexedCompany.getStreet() + " " + indexedCompany.getPostalCode() + "\r\n");
+		bufferedWriter.write("EMAIL:" + indexedCompany.getEmail() + "\r\n");
+		bufferedWriter.write("URL:" + indexedCompany.getUrl() + "\r\n");
+		bufferedWriter.write("END:VCARD");
+		bufferedWriter.close();
+
+		try {
+			InputStream inputStream = new FileInputStream(file);
+			response.setContentType("text/x-vcard;charset=utf-8");
+			IOUtils.copy(inputStream, response.getOutputStream());
+			response.flushBuffer();
+		} catch (FileNotFoundException fileNotFoundException) {
+			fileNotFoundException.printStackTrace();
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+
 
 		return str.toString();
 	}
@@ -59,7 +178,6 @@ public class VCardGetter {
 		String str = "";
 		int indexOf = json.indexOf(field);
 		str = json.substring(indexOf + field.length() + 3, json.indexOf("\"",indexOf + field.length() + 3));
-//		System.out.println(str);
 		return str;
 	}
 
